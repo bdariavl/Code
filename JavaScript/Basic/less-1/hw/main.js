d9779cf3187d41d6df5ca8463a303950dd52cd2e
bdariavl <bondarenkodariavl@gmail.com>
2024-01-23 11:53:21 +0200

    hw less-1

---
+++ JavaScript/Basic/less-1/hw/main.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 7;
+let c = 3;
+let d = 9;
+
+const sum = (a + b + c + d) / 4;
+
+console.log (sum);
+
+const part1 = "F34 ";
+const part2 = "is ";
+const part3 = "the best!"
+
+console.log(part1 + part2 + part3);
