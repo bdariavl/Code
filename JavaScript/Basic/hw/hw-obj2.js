a72d0fce651ea5bda38de6fa1bbcac9f1917fb21
bdariavl <bondarenkodariavl@gmail.com>
2024-02-27 00:15:36 +0200

    obj-2 hw

--- JavaScript/Basic/hw/hw-obj2.js
+++ JavaScript/Basic/hw/hw-obj2.js
@@ -1,11 +1,11 @@
 const data = {
-	a: 100,
+  a: 100,
   b: 300,
   c: 400,
-	d: 500,
-	e: 600,
-	f: 700,
-	g: 800
+  d: 500,
+  e: 600,
+  f: 700,
+  g: 800,
 };
 
 const lowData = {}; // до 300 (включно)
@@ -14,3 +14,20 @@ const bigData = {}; // після 300
 // За допомогою циклу опрацюйте данні так, щоб менші значення були у lowData,
 // більші у bigData
 
+
+function sortData(data) {
+	for (const key in data) {
+		let value = data[key];
+		if (value <= 300) {
+			lowData[key] = value;
+		} else {
+			bigData[key] = value;
+		}
+	}
+
+	return {lowData, bigData};
+}
+const result = sortData(data);
+
+console.log(result.lowData);
+console.log(result.bigData);
